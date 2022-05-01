ca060d004aa967c2f90dc7dedef26f9457a8bf99
Olga Khvostikova <Wireless-fidelity@yandex.ru>
2022-05-02 00:56:42 +0300

    Add terms duplicate check

--- main.kt
+++ main.kt
@@ -1,19 +1,43 @@
 package flashcards
 
+fun findKey(map: MutableMap<String, String>, value: String) : String {
+    for ((k, v) in map) {
+        if (v == value) {
+            return k
+        }
+    }
+    return ""
+}
+
 fun main() {
     println("Input the number of cards:")
     val n = readLine()!!.toInt()
-    val terms = MutableList(n) { "" }
-    val defs = MutableList(n) { "" }
+    var dict = mutableMapOf<String, String>()
     for (i in 0..n - 1) {
         println("Card #${i+1}")
-        terms[i] = readLine()!!
+        var term = readLine()!!
+        while (dict.contains(term)) {
+            println("The term \"$term\" already exists. Try again:")
+            term = readLine()!!
+        }
         println("The definition for card #${i+1}")
-        defs[i] = readLine()!!
+        var def = readLine()!!
+        while (findKey(dict, def) != "") {
+            println("The definition \"$def\" already exists. Try again:")
+            def = readLine()!!
+        }
+        dict[term] = def
     }
-    for (i in 0..n - 1) {
-        println("Print the definition of \"" + terms[i] + "\":")
+    for ((k, v) in dict) {
+        println("Print the definition of \"$k\":")
         val answer = readLine()!!
-        println(if (answer == defs[i]) "Correct!" else "Wrong. The right answer is \"" + defs[i] + "\".")
+        val key = findKey(dict, answer)
+        if (answer == v) {
+            println("Correct!")
+        } else if (key != "") {
+            println("Wrong. The right answer is \"$v\", but your definition is correct for \"$key\".")
+        } else {
+            println("Wrong. The right answer is \"$v\".")
+        }
     }
 }
